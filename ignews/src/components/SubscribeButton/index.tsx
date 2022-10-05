2d2aa0f23536e9515fac57e1facbe23ba3ccbd26
Yan Forni <yanfornimedeiros@gmail.com>
2022-10-05 17:51:56 -0300

    Getting Started Features of the Subscribe Button

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -1,15 +1,27 @@
-import styles from './styles.module.scss'
+import { signIn, useSession } from "next-auth/react";
+import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
-    priceId: string;
+  priceId: string;
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
-    return (
-        <button 
-        type="button"
-        className={styles.subscribeButton}>
-            Subscribe now
-        </button>
-    )
+  const {data: session} = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+  }
+
+  return (
+    <button
+      type="button"
+      onClick={() => handleSubscribe()}
+      className={styles.subscribeButton}
+    >
+      Subscribe now
+    </button>
+  );
 }
