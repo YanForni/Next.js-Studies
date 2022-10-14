f0d43d18819ae5978367e8e0db133f4bd2167b8a
Yan Forni <yanfornimedeiros@gmail.com>
2022-10-14 15:37:43 -0300

    Stripe working

--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -22,27 +22,33 @@ export default NextAuth({
 
       try {
         await fauna.query(
-          q.If(
-            q.Not(
-              q.Exists(
+          q.Let(
+            {
+              userExists: q.Exists(
                 q.Match(
-                  q.Index("user_by_email"), 
+                  q.Index("user_by_email"),
                   q.Casefold(user.email)
                 )
               )
-            ),
-            q.Create(
-              q.Collection("users"), 
-              { data: { email: email } }
-            ),
-            q.Get(
-              q.Match(
-                q.Index("user_by_email"), 
-                q.Casefold(user.email)
+            },
+            q.If(
+              q.Var('userExists'),
+              q.Get(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              ),
+              q.Create(
+                q.Collection('users'),
+                {
+                  data: { email }
+                }
               )
             )
           )
         )
+        
         return true;
       } catch {
         return false;
